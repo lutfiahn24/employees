bff3ad99abb6a1bed11b4ad308d12219515f1996
Lutfiahn <93109340+lutfiahn24@users.noreply.github.com>
2024-08-18 14:56:18 +0700

    Update EditEmployee.js

--- src/EditEmployee.js
+++ src/EditEmployee.js
@@ -1,56 +1,105 @@
-import axios from 'axios'
-import { useParams } from 'react-router-dom';
-import React, {useEffect, useState} from 'react'
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import './App.css';
 
 function EditEmployee() {
     const { id } = useParams();
     const [employee, setEmployee] = useState(null);
+    const [departments, setDepartments] = useState([]);
+    const navigate = useNavigate();
+    
     useEffect(() => {
-        axios.get('http://localhost:3000/employees/${id}')
+        // Fetch employee details
+        axios.get(`http://localhost:3000/employees/${id}`)
         .then(res => setEmployee(res.data))
-        .catch(er => console.log(er));
-    }, [id])
-    return(
+        .catch(err => console.log(err));
+
+        // Fetch departments for dropdown
+        axios.get('http://localhost:3000/departments')
+        .then(res => setDepartments(res.data))
+        .catch(err => console.log(err));
+    }, [id]);
+
+    const handleChange = (e) => {
+        setEmployee({ ...employee, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.put(`http://localhost:3000/employees/${id}`, employee)
+            .then(() => navigate('/'))
+            .catch(err => console.log(err));
+    };
+
+    if (!employee) return <div>Loading...</div>;
+
+    return (
         <div className='container'>
             <h1>Edit Employee's Data</h1>
-                <p>Employee ID : </p>
+            <form onSubmit={handleSubmit}>
                 <div className='content'>
                     <p>First Name</p>
-                    <input type='text' value={employee.first_name} className='form-control' placeholder='First Name'></input>
+                    <input
+                        type='text'
+                        name='first_name'
+                        value={employee.first_name}
+                        onChange={handleChange}
+                        className='form-control'
+                        placeholder='First Name'
+                    />
                 </div>
                 <div className='content'>
                     <p>Last Name</p>
-                    <input type='text' value={employee.last_name} className='form-control' placeholder='Last Name'></input>
+                    <input
+                        type='text'
+                        name='last_name'
+                        value={employee.last_name}
+                        onChange={handleChange}
+                        className='form-control'
+                        placeholder='Last Name'
+                    />
                 </div>
                 <div className='content'>
                     <p>Department</p>
-                    {/* IDK HOW */}
-                    <div className="input-group mb-3">
-                        <button className="btn btn-outline-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                        Choose the Department
-                        </button>
-                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <li><a className="dropdown-item" href="#">Action</a></li>
-                            <li><a className="dropdown-item" href="#">Another action</a></li>
-                            <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            <li><a className="dropdown-item" href="#">Separated link</a></li>
-                        </ul>
-                    </div>
+                    <select
+                        name='department_id'
+                        value={employee.department_id}
+                        onChange={handleChange}
+                        className='form-control'
+                    >
+                        {departments.map(dept => (
+                            <option key={dept.department_id} value={dept.department_id}>
+                                {dept.department_name}
+                            </option>
+                        ))}
+                    </select>
                 </div>
                 <div className='content'>
                     <p>Salary</p>
-                    <input type='text' value={employee.salary} className='form-control' placeholder='Ex: 50000'></input>
+                    <input
+                        type='text'
+                        name='salary'
+                        value={employee.salary}
+                        onChange={handleChange}
+                        className='form-control'
+                        placeholder='Ex: 50000'
+                    />
                 </div>
-
                 <div className='content'>
                     <p>Hire Date</p>
-                    <input type='date' value={employee.hire_date} className='form-control'></input>
+                    <input
+                        type='date'
+                        name='hire_date'
+                        value={employee.hire_date.split('T')[0]} // Format date to YYYY-MM-DD
+                        onChange={handleChange}
+                        className='form-control'
+                    />
                 </div>
-
-            <button type='submit' className='btn btn-primary btn-sm me-2'>Save</button>
+                <button type='submit' className='btn btn-primary btn-sm me-2'>Save</button>
+            </form>
         </div>
-    )
+    );
 }
 
 export default EditEmployee;
